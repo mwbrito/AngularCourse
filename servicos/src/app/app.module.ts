fb8607a94ca0b6582326f69dc39cadda03dfc6a7
unknown <mwbrito@gmail.com>
2020-04-15 02:42:41 -0300

    services

---
+++ servicos/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
+import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CriarCursoModule,
+    CursosModule
+  ],
+  //providers: [CursosService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
